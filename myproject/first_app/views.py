ef2109c87f80b7cb929862a51e5290b409cdb23a
MoniMonkata126126 <simeon.k.simeonov06@gmail.com>
2022-03-12 01:50:12 +0200

    Site can change manus, from home to quiz and vice versa

--- myproject/first_app/views.py
+++ myproject/first_app/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'Home_Hacktues.html')
+
+def quiz(request):
+    quiz_msg = 'How old are you?'
+    return render(request, 'quiz.html', {'quiz_msg': quiz_msg})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-import first_app
-from first_app.views import index
+from django.urls import path
+from first_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', views.index),
+    path('quiz', views.quiz)
 ]
