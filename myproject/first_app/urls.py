48dbce409955e2e98b71f006f49eedae77c6725e
MoniMonkata126126 <simeon.k.simeonov06@gmail.com>
2022-03-13 07:55:02 +0200

    Final commit

--- myproject/first_app/urls.py
+++ myproject/first_app/urls.py
@@ -1,19 +1,11 @@
 from argparse import Namespace
 from django.urls import path 
 from . import views
-from .views import (
-    QuizListView,
-    quiz_view
-)
 
 app_name = 'first_app'
 
 urlpatterns = [
     path('', views.index, name='homepage'),
-    path('pick_quiz', QuizListView.as_view(), name='pick_a_quiz'),
+    path('quiz', views.quiz, name='view-quiz'),
     path('info', views.info, name='info-view'),
-    path('pick_quiz/<pk>/', quiz_view, name='quiz-view'),
-
-    # path('pick_quiz', views.pick_quiz, name='pick_a_quiz'),
-    
 ]
--- myproject/first_app/views.py
+++ myproject/first_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
 from .models import Quiz
 
 
@@ -11,10 +10,6 @@ def index(request):
 def info(request):
     return render(request, 'info.html')
 
-class QuizListView(ListView):
-    model = Quiz
-    template_name = 'quiz/pick_quiz.html'
-
-def quiz_view(request, pk):
-    quiz = Quiz.objects.get(pk=pk)
-    return render(request, 'quiz.html', {'obj': quiz})
+def quiz(request):
+    context = Quiz.objects.all()
+    return render(request, 'quiz.html', {'Quiz': context})
